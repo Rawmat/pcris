cb960b886500a6c7b18ccdfe9ea79578607de0d1
cristiano <cristiano@example.org>
2020-06-17 22:43:40 +0200

    english

--- predict.c
+++ predict.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-/* Esempio di NN 
- * Dati input e tramite i weights si fa prediction
- * solo la forward propagation
+/* Example for an NN
+ * Inputs goes trough weight and gives predictions
+ * weights act as a volume knob
+ * input is know information
+ * weight is network knowledge
+ * here we have the predict step 1/3
+ * This is forward propagation
+ * Predict / Compare / Learn
  */
 
-
-
-/* un input e un predict 
- */
+/* One input and one prediction */
 void predict_1_1(){
    float in[] = { 1.0, 0.5, 0.2 };
    float w = 0.8;
    float pred;
 
-   /* una singola informazione (input) scalata con il peso
-    * restituisce il predict */
+   /* only one known information (input) scaled with the weight
+    * gives the prediction */
    pred = in[0] * w;
 
    printf("Input: %.2f Weight: %.2f Prediction %.2f\n", in[0], w, pred);
@@ -23,21 +25,22 @@ void predict_1_1(){
 
 
 
-/* Tre input e un predict 
- * piu accurata di 1 1
+/* Three inputs and one predict
+ * more inputs (information) gives more information
+ * then more accurate prediction
  */
 void predict_3_1(){
    float in[] = { 1.0, 0.5, 0.2 };
    float w[] = { 0.8, 0.6, 1.2 };
    float p1, p2, p3, pred;
 
-   /* predict singoli dagli input differenti con pesi differenti */
+   /* Single prediction for the different information (input) with own weight */
    p1 = in[0] * w[0];
    p2 = in[1] * w[1];
    p3 = in[2] * w[2];
 
-   /* somma pesata, i tre predict locali con sensibilita diversa (pesi) vengono sommati
-    * per contribuire alla predict */
+   /* Weighted sum - the three independent prediction  summed to contribute 
+    * to the single prediction */
    pred = p1 + p2 + p3;
 
    printf("Input1: %.2f * Weight1 %2f = %.2f\n", in[0], w[0], p1);
