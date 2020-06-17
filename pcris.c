32f421d5ee23eccfeafb4be0f22f19e9994cd1e4
cristiano <cristiano@example.org>
2020-06-18 00:14:57 +0200

    rename

--- pcris.c
+++ pcris.c
@@ -20,6 +20,7 @@ void predict_1_1(){
     * gives the prediction */
    pred = in[0] * w;
 
+   printf("ONE SINGLE PREDICTION\n");
    printf("Input: %.2f Weight: %.2f Prediction %.2f\n", in[0], w, pred);
 }
 
@@ -43,17 +44,47 @@ void predict_3_1(){
     * to the single prediction */
    pred = p1 + p2 + p3;
 
-   printf("Input1: %.2f * Weight1 %2f = %.2f\n", in[0], w[0], p1);
-   printf("Input2: %.2f * Weight2 %2f = %.2f\n", in[1], w[1], p2);
-   printf("Input3: %.2f * Weight3 %2f = %.2f\n", in[2], w[2], p3);
-   printf("Pred = %2f + %.2f + %.2f = %.2f\n", p1, p2, p3, pred);
+   printf("ONE SINGLE PREDICTION WITH THREE INPUT CONTRIBUTION\n");
+   printf("Input1: %.2f * Weight1 %.2f = %.2f\n", in[0], w[0], p1);
+   printf("Input2: %.2f * Weight2 %.2f = %.2f\n", in[1], w[1], p2);
+   printf("Input3: %.2f * Weight3 %.2f = %.2f\n", in[2], w[2], p3);
+   printf("Pred = %.2f + %.2f + %.2f = %.2f\n", p1, p2, p3, pred);
+}
+
+/* One input and three prediction (different responses)
+ */
+void predict_1_3(){
+   float in[] = { 1.0, 0.5, 0.2 };
+   float w[] = { 0.8, 0.6, 1.2 };
+    float p1, p2, p3;
+
+   /* Three different prediction with the same information (input)
+    * this are really a three single predictions
+    */
+   p1 = in[0] * w[0];
+   p2 = in[0] * w[1];
+   p3 = in[0] * w[2];
+
+   /* Weighted sum - the three independent prediction summed to contribute 
+    * to the single prediction */
+   printf("THREE SEPARATE PREDICTION WITH THE SAME INPUT INFORMATION WITH DIFFERENT WEIGHT\n");
+   printf("Input1: %.2f * Weight1 %.2f Pred1 = %.2f\n", in[0], w[0], p1);
+   printf("Input1: %.2f * Weight2 %.2f Pred2 = %.2f\n", in[0], w[1], p2);
+   printf("Input1: %.2f * Weight3 %.2f Pred3 = %.2f\n", in[0], w[2], p3);
+
 }
 
 
 void main(){
    printf("\n");
+
    predict_1_1();
    printf("\n");   
+
    predict_3_1();
+   printf("\n");
+
+   predict_1_3();
    printf("\n");      
+   
 }
