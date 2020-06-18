e9c0411f7040b097a4bfa5566e24abe96990ccd8
rawmat <cristiano@example.org>
2020-06-18 02:20:39 +0200

    added learning two methods

--- pcris.c
+++ pcris.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
 /* Example for an NN
- * Inputs goes trough weight and gives predictions
- * weights act as a volume knob
- * input is know information
- * weight is network knowledge
- * here we have the predict step 1/3
- * This is forward propagation
+ *
  * Predict / Compare / Learn
  */
 
@@ -56,7 +51,7 @@ void predict_3_1(){
 void predict_1_3(){
    float in[] = { 1.0, 0.5, 0.2 };
    float w[] = { 0.8, 0.6, 1.2 };
-    float p1, p2, p3;
+   float p1, p2, p3;
 
    /* Three different prediction with the same information (input)
     * this are really a three single predictions
@@ -75,6 +70,117 @@ void predict_1_3(){
 }
 
 
+
+/* hot/cold learning */
+void learn_hot_cold(){
+   float in = -1.0;
+   float w = 0.8;
+   float pred, pred_p, pred_m;
+   float goal = 2.0;
+   float re, re_p, re_m;
+   float error, err, err_p, err_m;
+   float hl = 0.001;
+   int i;
+
+   for(i = 0; i < 300; i++){
+
+      /* tre step 
+       * 1 - normal prediction
+       * 2 - prediction with weight plus
+       * 3 - prediction with weight minus
+       * choose the one with lower error
+       * error is prediction minus goal
+       * (squared) for gives priority to higher error and
+       * and when averaging to not consider opposite miss same magnitude
+       *
+       * hot learning is trying without considering error of the prediction
+       * without knowing direction, only by trying
+       * and the fixed increase can make not reaching the goal, only oscillate around
+       */
+
+      {
+	 /* normal prediction */
+	 pred = in * w; 
+	 re = (pred - goal );
+	 err = re * re;
+
+	 /* calculate network learning loop error */
+	 error = (pred - goal) * (pred - goal);
+	 printf("Cycle: %d      Prediction: %f      Error: %f\n", i, pred, error);
+	    
+      }
+      
+      {
+	 /* prediction with wieght plus */
+	 pred_p = in * (w + hl); 
+	 re_p = (pred_p - goal );
+	 err_p = re_p * re_p;
+      }
+
+      {
+	 /* prediction with wieght minus */
+	 pred_m = in * (w - hl); 
+	 re_m = (pred_m - goal );
+	 err_m = re_m * re_m;
+      }
+
+      // choose who do best
+      if ( (err > err_p) || (err > err_m) )
+	 if (err_p < err_m)
+	    w += hl;
+	 else
+	    w-= hl;
+   }
+}
+
+
+/* gradient descent learning */
+void learn_hot_gd(){
+   float in = -1.0;
+   float w = 0.8;
+   float pred;
+   float goal = 2.0;
+   float delta_w;
+   float error;
+   float alpha = 0.8;
+   int i;
+
+   for(i = 0; i < 200; i++){
+
+      /* 
+       * error is prediction minus goal
+       * (squared) for gives priority to higher error and
+       * and when averaging to not consider opposite miss same magnitude
+       */
+
+      {
+	 /* normal prediction */
+	 pred = in * w; 
+
+	 /* calculate delta weight - direction and amount 
+	  * multiplying by input performs - stopping, negative reversal, scaling 
+	  */
+	 delta_w = (pred - goal ) * in ;
+
+	 /* factor */
+	 delta_w *= alpha;
+	 
+	 w -= delta_w;
+	 
+	 /* calculate network learning loop error */
+	 error = (pred - goal) * (pred - goal);
+	 printf("Cycle: %d      Prediction: %f      Error: %f\n", i, pred, error);
+	    
+      }
+
+      
+      
+
+   }
+}
+
+
+
 void main(){
    printf("\n");
 
@@ -85,6 +191,11 @@ void main(){
    printf("\n");
 
    predict_1_3();
-   printf("\n");      
-   
+   printf("\n");
+
+   learn_hot_cold();
+   printf("\n");   
+
+   learn_hot_gd();
+   printf("\n");   
 }
